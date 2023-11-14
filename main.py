70fb16573b4cc0a3f079b9c149e45db70bb34a56
Alex <sashok7976@bk.ru>
2023-11-14 20:11:22 +0500

    commit

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-def my_first_repositorii():
-    print('My', end=' ')
-    print('first', end=' ')
-    print('get', end='-')
-    print('repo', end='')
-my_first_repositorii()
+from PyQt5 import QtGui, uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtCore import Qt
+from random import randint
+
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('untitled.ui', self)
+        self.pushButton.clicked.connect(self.draw_circle)
+
+    def paintEvent(self, event):
+        painter = QPainter()
+        painter.begin(self)
+
+
+    def draw_circle(self, painter):
+        rnd = randint(1, 200)
+        print(rnd)
+        painter.setBrush(QColor(255, 255, 0))
+        painter.drawEllipse(390, 270, rnd, rnd)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
